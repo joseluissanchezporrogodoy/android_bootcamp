c99038c8136f6a2b4c83607632bf746c32e1b034
JoseLuisSPG <joseluis.sanchezporro.godoy@gmail.com>
2017-10-30 23:47:02 +0100

    First commit

---
+++ app/src/main/java/com/joseluissanchez_porrogodoy/guedr/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.joseluissanchez_porrogodoy.guedr
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    val TAG = MainActivity::class.java.canonicalName
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.v(TAG,"He pasado por onCreate")
+    }
+}
